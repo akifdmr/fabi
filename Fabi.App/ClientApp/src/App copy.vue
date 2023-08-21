3948bb0d72a5051b8fb9feb93adb2ab1d8ec17d0
akifdmr <akifdmr@gmail.com>
2023-08-21 04:30:51 +0300

    take
    take

---
+++ Fabi.App/ClientApp/src/App copy.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { createRouter, createWebHistory } from 'vue-router'
+import { routesArr } from './router'
+import './charts/ChartjsConfig';
+
+const baseUrl =import.meta.env.BASE_URL;
+
+const router = createWebHistory({
+    history: routerHistory,
+    basename: baseUrl,
+    routesArr
+});
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => router.dispose())
+}
+
+export default function App() {
+  return <RouterProvider router={router} fallbackElement={<Fallback />} />
+}
+
+export function Fallback() {
+  return <p>Performing initial data load</p>
+}
+</script>
+
